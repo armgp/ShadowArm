d6cdf7e3ff5b37aaca2df33f8e02e128fe1aa620
armgp <49094298+armgp@users.noreply.github.com>
2020-01-05 13:09:19 +0530

    hands up and down detection

--- Phase2/p5js-clown nose project/main.py
+++ Phase2/p5js-clown nose project/main.py
@@ -7,21 +7,32 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def sessions():
-    print('message was received!!!')
+    print('INITIALIZING....')
     return render_template('index.html')
 
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
-
 @socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    # print('received my event: ' + str(json))
-    res = str(json)
-    # print(json)
-    print("x ",json['val']['pose']['nose']['x'])
-    print("y ",json['val']['pose']['nose']['y'])
-    print("confidence ",json['val']['pose']['nose']['confidence'])
+def handle_my_custom_event(json):
+
+    rightelbowY = json['val']['pose']['keypoints'][8]['position']['y']
+    rightshoulderY = json['val']['pose']['keypoints'][6]['position']['y']
+    confidenceRE = json['val']['pose']['keypoints'][8]['score']
+    confidenceRS = json['val']['pose']['keypoints'][6]['score']
+
+    # rightelbowY = json['val']['pose']['rightElbow']['y']
+    # rightshoulderY = json['val']['pose']['rightShoulder']['y']
+    # confidenceRE = json['val']['pose']['rightElbow']['confidence']
+    # confidenceRS = json['val']['pose']['rightShoulder']['confidence']
+
+    # print(" reY : ",rightelbowY)
+    # print(" rsY : ",rightshoulderY)
+    # print(" confidenceRE : ",confidenceRE)
+    # print(" confidenceRS : ",confidenceRS)
+
+    if rightelbowY < rightshoulderY:
+        print('HANDS UP !!!')
+    else:
+        print('hands down ...')
     
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app)
